f0976a75112ac59f9212c2677d3f2196d13550fd
zktucker <zktucker@byui.edu>
2025-04-26 01:10:14 -0700

    v1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+         string fname = Console.ReadLine();
+        Console.WriteLine("What is your last name?"); 
+        string lname = Console.ReadLine();
+        Console.WriteLine($"You name is {lname}, {fname} {lname}.");
     }
 }
